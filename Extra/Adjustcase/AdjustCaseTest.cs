92a02bd8bbe9079aaba52d92b685fa5de9f3a2bf
jesper svensson <jesper.svensson@biltema.com>
2021-02-16 12:16:07 +0100

    adjustcasetest

---
+++ Extra/Adjustcase/AdjustCaseTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System;
+using System.Globalization;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace Extra
+{
+    [TestClass]
+    public class AdjustCaseTest
+    {
+        [TestCase("rhino", "cAt", "rHino")]
+        [TestCase("cat", "RHino", "CAt")]
+        [TestCase("rhino", "aAaaA", "rHinO")]
+        [TestCase("RHINO", "aAaaA", "rHinO")]
+        [TestCase("RHINO", "aAaaAaAaAaA", "rHinO")]   // längre template
+        [TestCase("RHINOoOoO", "aAaaA", "rHinOoOoO")] // längre ord
+        public void adjust_case(string word, string template, string expected)
+        {
+            Assert.AreEqual(expected, AdjustCase(word, template));
+        }
+
+        private string AdjustCase(string word, string template)
+        {
+            //todo sloppy
+            var cut = word.Length <= template.Length ? word.Length : template.Length;
+            var start = word.Substring(0, cut);
+            var rest = word.Substring(cut);
+            var newStart = new char[cut];
+            for (int i = 0; i < start.Length; i++)
+            {
+                var w = start[i];
+                var t = template[i];
+                if (char.IsUpper(w) && char.IsLower(t))  w = char.ToLower(w);
+                else if (char.IsLower(w) && char.IsUpper(t))  w = char.ToUpper(w);
+                newStart[i] = w;
+            }
+
+            return $"{new string(newStart)}{rest}";
+        }
+}
